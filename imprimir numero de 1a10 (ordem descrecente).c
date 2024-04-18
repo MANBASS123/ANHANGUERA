8bce24b8a06fd99f590163a84a3d514efff18dfa
MANBASS123 <165338203+MANBASS123@users.noreply.github.com>
2024-04-18 20:52:47 -0300

    Add files via upload

---
+++ imprimir numero de 1a10 (ordem descrecente).c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+    // Inicializa a variável i com 10
+    int i = 10;
+    
+    // Loop enquanto i for maior ou igual a 1
+    while (i >= 1) {
+        // Imprime o valor de i
+        printf("%d\n", i);
+        // Decrementa i
+        i--;
+    }
+    
+    return 0;
+}
+
+
